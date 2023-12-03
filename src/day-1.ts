4d54eaa07f3fc07d9678c4a546555903deaa1250
Richard Crosby <richard.crosby@protonmail.com>
2023-12-03 22:41:51 +0100

    day 1 part 2

--- src/day-1.ts
+++ src/day-1.ts
@@ -1,7 +1,20 @@
 import { readInput } from "./utils";
 
-(async function main() {
+(async function part1() {
   let sum = 0;
-  for await (const line of readInput('/day-1-input.txt')) sum += Number(line.charAt(line.search(/[0-9]/)) + line.charAt(line.length - 1 - line.split('').reverse().join('').search(/[0-9]/)));
-  console.log(sum);
+  for await (const line of readInput('/day-1-input.txt')) {
+    const {0:p1, length:l, [l-1]:p2} = line.match(/\d/g) as string[];
+    sum += Number(p1 + p2);
+  }
+  console.log(`Part 1: ${sum}`);
+})();
+
+(async function part2() {
+  let sum = 0;
+  for await (let line of readInput('/day-1-input.txt')) {
+    const {0:p1, length:l, [l-1]:p2} = Array.from(line.matchAll(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g), (m: any) => m[1]) as string[];
+    const r = (p: string) => String({ one:1, two:2, three:3, four:4, five:5, six:6, seven:7, eight:8, nine:9 }[p] || p);
+    sum += Number(r(p1) + r(p2));
+  }
+  console.log(`Part 2: ${sum}`);
 })();
